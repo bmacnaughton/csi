1f2fa943f2d3f443a23639c0b1990cf2f21d44d5
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-12-12 18:00:57 -0800

    cleanup start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,32 +1,37 @@
 #!/usr/bin/bash
 
+#
+# provides defaults for key options. Others can be used added with env vars.
+# see the server's configuration.js files.
+#
 
 server=$1
 pretend=$2
 
 case $server in
   "koa-app")
-    app_port=${APP_PORT:-3000}
-    app_be_ip=${APP_BE_IP-localhost:4000/api}
+    x=${APP_PORT:=3000}
+    x=${APP_BE_IP=localhost:4000/api}
 
-    [ -n "$app_be_ip" ] && be_text=" (backend $app_be_ip)"
+    [ -n "$APP_BE_IP" ] && be_text=" (backend $APP_BE_IP)"
 
     if [ -z "$pretend" ]; then
-      echo "starting website at localhost:${app_port}${be_text}"
-      APP_PORT=$csi_port APP_BE_IP=$app_be_ip node koa-app/index.js
+      echo "starting website at localhost:${APP_PORT}${be_text}"
+      APP_PORT=$APP_PORT APP_BE_IP=$APP_BE_IP node koa-app/index.js
     else
-      echo "pretending to start site at localhost:${app_port}${be_text}"
+      echo "pretending to start site at localhost:${APP_PORT}${be_text}"
     fi
   ;;
 
   "csi-server")
-    csi_port=${CSI_PORT:-4000}
+    # shellcheck disable=SC2034
+    x=${CSI_PORT=4000}
 
     if [ -z "$pretend" ]; then
-      echo "starting backend at localhost:${csi_port}"
-      CSI_PORT=$csi_port node csi-server/index.js
+      echo "starting backend at localhost:${CSI_PORT}"
+      CSI_PORT=$CSI_PORT node csi-server/index.js
     else
-      echo "pretending to start backend at localhost:${csi_port}"
+      echo "pretending to start backend at localhost:${CSI_PORT}"
     fi
   ;;
 
